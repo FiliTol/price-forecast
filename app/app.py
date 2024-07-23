576a67d0b042fb3308a4de75aa670da1fbb06008
FiliTol <filippotolin@protonmail.com>
2024-07-23 22:21:58 +0200

    # Update
    - Datacleaning and handling for December2023
    - Draft for missing values Imputation with KNN
    - minor change to streamlit app
    - minor changes

--- app/app.py
+++ app/app.py
@@ -4,6 +4,11 @@ import random
 
 random.seed(874631)
 
+# The main idea is to create two pages for the app:
+# - one for the visualization of current dataset characteristics, providing temporal filters and other info
+# - one for the actual prediction, providing empty slots for input data and some KPIs for the results
+
+
 st.set_page_config(page_title="Price Prediction App")
 hide_default_format = """
        <style>
@@ -23,12 +28,12 @@ def random_color_generator():
 
 random_color = random_color_generator()
 
-df = pd.read_csv("data/listings.csv")
-graph = df[['id', 'host_id', 'latitude', 'longitude', 'host_neighbourhood', 'host_response_time']]
+df = pd.read_csv("data/2023dic/d_listings.csv")
+graph = df[['id', 'host_id', 'latitude', 'longitude', 'neighbourhood_cleansed', 'host_response_time']]
 
 zona = st.sidebar.selectbox(
     "Which sestiere do you want to see?",
-    graph['host_neighbourhood'].unique().tolist()
+    graph['neighbourhood_cleansed'].unique().tolist()
 )
 
 response_time = st.sidebar.radio('Host answers...',
@@ -36,11 +41,11 @@ response_time = st.sidebar.radio('Host answers...',
                                  horizontal=True)
 
 col_zona = {}
-for i in graph['host_neighbourhood'].unique():
+for i in graph['neighbourhood_cleansed'].unique():
     col_zona[i] = random_color_generator()
 
-graph['color'] = graph['host_neighbourhood'].apply(lambda x: col_zona.get(x))
+graph['color'] = graph['neighbourhood_cleansed'].apply(lambda x: col_zona.get(x))
 
-st.map(graph[(graph['host_neighbourhood'] == zona) & (graph['host_response_time'] == response_time)],
+st.map(graph[(graph['neighbourhood_cleansed'] == zona)], # & (graph['host_response_time'] == response_time)],
        color='color',
        size=5)

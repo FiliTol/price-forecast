86fbacd30994a5ab48ce57e22abfa83569da9b2b
FiliTol <filippotolin@protonmail.com>
2024-07-31 14:39:41 +0200

    # Code refactoring
    - Added custom transformer for Geographic features creation
    - Added new pipeline at the bottom for Feature engineering of multiple columns
    - minor changes

--- scripts/custom/transformers.py
+++ scripts/custom/transformers.py
@@ -10,7 +10,6 @@ class GeographicTransformer(BaseEstimator, TransformerMixin):
     # https://datascience.stackexchange.com/questions/117200/creating-new-features-as-linear-combination-of-others-as-part-of-a-scikit-learn
     # https://www.andrewvillazon.com/custom-scikit-learn-transformers/
     def __init__(self, locations: dict, column: str = "host_location"):
-
         self.column = column
         self.locations = locations
 
@@ -18,17 +17,11 @@ class GeographicTransformer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X: pd.DataFrame, y=None):
-        if self.column == "host_location":
-            X = self.transform_to_coordinates(X, self.locations)
-            X[self.column] = X.apply(lambda row: self.geodesic_distancer(row, from_loc="host_location"))
-            return X
-        else:
-            X = self.create_strategic_locations_features(X)
-            X = self.apply_location_to_feature(X)
-            X = self.apply_distancer_to_strategic_locations(X)
-            return X
+        X = self.transform_to_coordinates(X)
+        X[self.column] = X.apply(lambda row: self.geodesic_distancer(row, from_loc="host_location"), axis=1)
+        return X
 
-    def transform_to_coordinates(self, X, locations: dict):
+    def transform_to_coordinates(self, X):
         """
         Given an entry and a dictionary, returns the latitude, longitude for
         the entry that are saved in the dictionary
@@ -37,7 +30,7 @@ class GeographicTransformer(BaseEstimator, TransformerMixin):
         :return: [latitude, longitude]
         """
         try:
-            X[self.column] = X[self.column].apply(lambda x: locations.get(x))
+            X[self.column] = X[self.column].apply(lambda x: self.locations.get(x))
             return X
         except:
             return X
@@ -50,39 +43,6 @@ class GeographicTransformer(BaseEstimator, TransformerMixin):
             return geodesic(coords_1, coords_2).km
         except:
             return None
-
-    @staticmethod
-    def create_strategic_locations_features(X: pd.DataFrame) -> pd.DataFrame:
-        X["airport_distance_km"] = None
-        X["ferretto_square_distance_km"] = None
-        X["roma_square_distance_km"] = None
-        X["rialto_bridge_distance_km"] = None
-        X["san_marco_square_distance_km"] = None
-        return X
-
-    def apply_location_to_feature(self, X: pd.DataFrame) -> pd.DataFrame:
-        X["airport_distance_km"] = X["airport_distance_km"].apply(lambda x: self.locations["Aeroporto Marco Polo"])
-        X["ferretto_square_distance_km"] = X["ferretto_square_distance_km"].apply(
-            lambda x: self.locations["Piazza Erminio Ferretto"])
-        X["roma_square_distance_km"] = X["roma_square_distance_km"].apply(lambda x: self.locations["Piazzale Roma"])
-        X["rialto_bridge_distance_km"] = X["rialto_bridge_distance_km"].apply(
-            lambda x: self.locations["Ponte di Rialto"])
-        X["san_marco_square_distance_km"] = X["san_marco_square_distance_km"].apply(
-            lambda x: self.locations["Piazza San Marco"])
-        return X
-
-    def apply_distancer_to_strategic_locations(self, X: pd.DataFrame) -> pd.DataFrame:
-        X['airport_distance_km'] = X.apply(lambda row: self.geodesic_distancer(row=row, from_loc="airport_distance_km"),
-                                           axis=1)
-        X['ferretto_square_distance_km'] = X.apply(
-            lambda row: self.geodesic_distancer(row=row, from_loc="ferretto_square_distance_km"), axis=1)
-        X['roma_square_distance_km'] = X.apply(
-            lambda row: self.geodesic_distancer(row=row, from_loc="roma_square_distance_km"), axis=1)
-        X['rialto_bridge_distance_km'] = X.apply(
-            lambda row: self.geodesic_distancer(row=row, from_loc="rialto_bridge_distance_km"), axis=1)
-        X['san_marco_square_distance_km'] = X.apply(
-            lambda row: self.geodesic_distancer(row=row, from_loc="san_marco_square_distance_km"), axis=1)
-        return X
 
 
 class VectorToDataFrame(BaseEstimator, TransformerMixin):
@@ -150,13 +110,38 @@ class BathroomsTransformer(BaseEstimator, TransformerMixin):
 
 
 class CreateStrategicLocationTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, locations: dict):
+        self.locations = locations
+
     def fit(self, X, y=None):
         return self
 
     def transform(self, X, y=None):
-        # Perform arbitary transformation
-        X["random_int"] = randint(0, 10, X.shape[0])
+        X = self.allocate_features(X)
+        X = self.apply_distancer_to_strategic_locations(X)
         return X
+
+    def allocate_features(self, X):
+        X["airport_distance_km"] = pd.Series([self.locations["Aeroporto Marco Polo"]] * X.shape[0])
+        X["ferretto_square_distance_km"] = pd.Series([self.locations["Piazza Erminio Ferretto"]] * X.shape[0])
+        X["roma_square_distance_km"] = pd.Series([self.locations["Piazzale Roma"]] * X.shape[0])
+        X["rialto_bridge_distance_km"] = pd.Series([self.locations["Ponte di Rialto"]] * X.shape[0])
+        X["san_marco_square_distance_km"] = pd.Series([self.locations["Piazza San Marco"]] * X.shape[0])
+        return X
+    
+    def apply_distancer_to_strategic_locations(self, X: pd.DataFrame) -> pd.DataFrame:
+        X['airport_distance_km'] = X.apply(lambda row: self.geodesic_distancer(row=row, from_loc="airport_distance_km"), axis=1)
+        X['ferretto_square_distance_km'] = X.apply(lambda row: self.geodesic_distancer(row=row, from_loc="ferretto_square_distance_km"), axis=1)
+        X['roma_square_distance_km'] = X.apply(lambda row: self.geodesic_distancer(row=row, from_loc="roma_square_distance_km"), axis=1)
+        X['rialto_bridge_distance_km'] = X.apply(lambda row: self.geodesic_distancer(row=row, from_loc="rialto_bridge_distance_km"), axis=1)
+        X['san_marco_square_distance_km'] = X.apply(lambda row: self.geodesic_distancer(row=row, from_loc="san_marco_square_distance_km"), axis=1)
+        return X
+
+    @staticmethod
+    def geodesic_distancer(row, from_loc: str):
+        coords_1 = (row[from_loc][0], row[from_loc][1])
+        coords_2 = (row["latitude"], row["longitude"])
+        return geodesic(coords_1, coords_2).km
 
 
 class CreateVerificationsTransformer(BaseEstimator, TransformerMixin):
@@ -165,7 +150,7 @@ class CreateVerificationsTransformer(BaseEstimator, TransformerMixin):
 
     def transform(self, X, y=None):
         # Perform arbitary transformation
-        X["random_int"] = randint(0, 10, X.shape[0])
+        #X["random_int"] = randint(0, 10, X.shape[0])
         return X
 
 
@@ -175,7 +160,7 @@ class CreateBathsTransformer(BaseEstimator, TransformerMixin):
 
     def transform(self, X, y=None):
         # Perform arbitary transformation
-        X["random_int"] = randint(0, 10, X.shape[0])
+        #X["random_int"] = randint(0, 10, X.shape[0])
         return X
 
 
